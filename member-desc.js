9d504004e92ce3d36961411a0610c31b691be7f5
ClawlessTiger <aarush.joglekar@gmail.com>
2024-08-14 10:23:03 -0700

    scrollable in member info page, more info link fixed

--- member-desc.js
+++ member-desc.js
@@ -22,40 +22,44 @@ $.get(
       data["member"].officialWebsiteUrl +
       " target='_blank'>" +
       data["member"].officialWebsiteUrl +
-      "</a><br> <b>Terms:</b>";
+      "</a><br> <b>Terms:</b>\
+      <div style='overflow-y: auto; height: 30rem;'>";
     for (term in data["member"].terms) {
-      console.log(term);
-      if (data["member"].terms[term].endYear) {
-        var endYearVariable = data["member"].terms[term].endYear;
+      let termArray = data["member"]["terms"]
+      let currentTerm = termArray[termArray.length - term - 1]
+      if (currentTerm.endYear) {
+        var endYearVariable = currentTerm.endYear;
       } else {
         var endYearVariable = "Present";
       }
       let termHTML =
         "<div class='card my-2'>\
   <h5 class='card-header'>" +
-        data["member"].terms[term].startYear +
+        currentTerm.startYear +
         " - " +
         endYearVariable +
         "</h5>\
   <div class='card-body'>\
     <h5 class='card-title'>" +
-        data["member"].terms[term].chamber +
+        currentTerm.chamber +
         "</h5>\
     <p class='card-text'><b>State: </b>" +
-        data["member"].terms[term].stateName +
+        currentTerm.stateName +
         "<br/><b>Congress Session: </b>" +
-        data["member"].terms[term].congress +
+        currentTerm.congress +
         "</p>\
   </div>\
 </div>";
 
-      memberInfoHtml = memberInfoHtml + termHTML;
+      memberInfoHtml += termHTML;
     }
+    memberInfoHtml += "</div>"
 
     $.get(
       "https://api.congress.gov/v3/member/" + memberID + "/sponsored-legislation?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA",
       function (data) {
-        let sponsoredLegislationHtml = "<p style='margin-top: 3.5em'><b>Sponsored Legislation (latest 250)</b></p>"
+        console.log(data);
+        let sponsoredLegislationHtml = "<p style='margin-top: 3.5em'><b>Sponsored Legislation (latest 250)</b></p><div style='overflow-y:auto; height:45rem;'>"
         for (legislation in data["sponsoredLegislation"]){
           let currentLegislation = data["sponsoredLegislation"][legislation]
           let currentLegislationCategory = currentLegislation["policyArea"].name
@@ -67,13 +71,14 @@ $.get(
             <div class='card-body'>\
               <h5 class='card-title'>" + currentLegislation.title + "</h5>\
               <p class='card-text'><b>Latest Action (" + currentLegislation["latestAction"].actionDate + "): </b>" + currentLegislation["latestAction"].text + "\
-              <br/><b>More Info: </b><a href=" + currentLegislation.url +">Click Here<a/>\
+              <br/><b>More Info: </b><a href='https://congress.gov/bill/" + currentLegislation.congress + "/" + currentLegislation.type.toLowerCase() +"/" + currentLegislation.number + "' target='_blank'>Click Here</a>\
               </p>\
             </div>\
           </div>"
 
-          sponsoredLegislationHtml = sponsoredLegislationHtml + newLegislationHtml
+          sponsoredLegislationHtml += newLegislationHtml
         }
+        sponsoredLegislationHtml += "</div>"
         $("#sponsoredLegislationHtml").append(sponsoredLegislationHtml);
       }
     
