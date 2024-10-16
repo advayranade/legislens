75c71d6a8d3b3d267e1b7b8acc109bdcd8e5e168
ClawlessTiger <aarush.joglekar@gmail.com>
2024-10-16 15:25:38 -0700

    broken into four requests

--- app.js
+++ app.js
@@ -2521,141 +2521,160 @@ $.ajax({
       currentDataset['summary'] = "<SUMMARIZE THIS BILL>"
       dataset.push(currentDataset);
     }
+    let dataset1 = dataset.slice(0, (dataset.length) / 4)
+    let dataset2 = dataset.slice((dataset.length) / 4, (dataset.length)/2)
+    let dataset3 = dataset.slice((dataset.length)/2, (dataset.length)/4*3);
+    let dataset4 = dataset.slice((dataset.length)/4*3, dataset.length);
     let prompt = "\
-    Provide a detailed but short paragraph summary for each of the following congressional bills. Return the summaries in a json array\n\n" + JSON.stringify(dataset)
-    model.generateContent(prompt).then((result) => {
-      let stringResponse = result.response.text()
-      let cleanedResponseString = stringResponse.replace(/```json|```/g, '');
-      let response = JSON.parse(cleanedResponseString)
-      for (let i = 0; i < data["bills"].length; i++) {
-        var billSummary;
-        var color1 = '#000000'
-        var color2 = '#000000'
-        let billInfoURL =
-          "https://api.congress.gov/v3/bill/" +
-          data["bills"][i]["congress"] +
-          "/" +
-          data["bills"][i]["type"].toLowerCase() +
-          "/" +
-          data["bills"][i]["number"] +
-          "/summaries?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
-        let sponsorURL =
-          "https://api.congress.gov/v3/bill/" +
-          data["bills"][i]["congress"] +
-          "/" +
-          data["bills"][i]["type"].toLowerCase() +
-          "/" +
-          data["bills"][i]["number"] +
-          "?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
-        $.get(sponsorURL, (res) => {
-          billData = res;
-          let sponsorId = res["bill"]["sponsors"][0].bioguideId;
-          let sponsorImgURL =
-            "https://api.congress.gov/v3/member/" +
-            sponsorId +
+    Provide a detailed but short paragraph summary for each of the following congressional bills. Return the summaries in a json array\n\n"
+    Promise.all([
+      model.generateContent(prompt + JSON.stringify(dataset1)),
+      model.generateContent(prompt + JSON.stringify(dataset2)),
+      model.generateContent(prompt + JSON.stringify(dataset3)),
+      model.generateContent(prompt + JSON.stringify(dataset4)),
+    ])
+      .then(([result1, result2, result3, result4]) => {
+        return Promise.all([result1.response.text(), result2.response.text(), result3.response.text(), result4.response.text()]); // Convert both responses to text
+      })
+      .then(([stringResponse1, stringResponse2, stringResponse3, stringResponse4]) => {
+        let cleanedResponseString1 = stringResponse1.replace(/```json|```/g, '');
+        let cleanedResponseString2 = stringResponse2.replace(/```json|```/g, '');
+        let cleanedResponseString3 = stringResponse3.replace(/```json|```/g, '');
+        let cleanedResponseString4 = stringResponse4.replace(/```json|```/g, '');
+        let response1 = JSON.parse(cleanedResponseString1);
+        let response2 = JSON.parse(cleanedResponseString2);
+        let response3 = JSON.parse(cleanedResponseString3);
+        let response4 = JSON.parse(cleanedResponseString4);
+        let finalResponse = response1.concat(response2).concat(response3).concat(response4)
+        for (let i = 0; i < data["bills"].length; i++) {
+          var billSummary;
+          var color1 = '#000000'
+          var color2 = '#000000'
+          let billInfoURL =
+            "https://api.congress.gov/v3/bill/" +
+            data["bills"][i]["congress"] +
+            "/" +
+            data["bills"][i]["type"].toLowerCase() +
+            "/" +
+            data["bills"][i]["number"] +
+            "/summaries?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
+          let sponsorURL =
+            "https://api.congress.gov/v3/bill/" +
+            data["bills"][i]["congress"] +
+            "/" +
+            data["bills"][i]["type"].toLowerCase() +
+            "/" +
+            data["bills"][i]["number"] +
             "?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
-          $.get(sponsorImgURL, (r) => {
-            var sponsorImg = r["member"]["depiction"].imageUrl;
-            $.get(billInfoURL, (resp) => {
-              if (data["bills"][i]["latestAction"]["actionDate"]) {
-                var actionDate = data["bills"][i]["latestAction"]["actionDate"];
-              }
-              let month = actionDate[5] + actionDate[6];
-              if (month[0] == 0) {
-                month = month[1];
-              }
-              let day = actionDate[8] + actionDate[9];
-              if (day[0] == 0) {
-                day = day[1];
-              }
-              let year =
-                actionDate[0] + actionDate[1] + actionDate[2] + actionDate[3];
-              actionDate = month + "/" + day + "/" + year;
-              if (resp["summaries"][0]) {
-                billSummary = resp["summaries"][0]["text"];
-              } else if (resp["summaries"]) {
-                billSummary = resp["summaries"]["text"];
-              }
-  
-              if (billSummary === undefined) {
-                color1 = '#6090FF'
-                color2 = '#FF76A1'
-                for (let bill in response){
-                  let currentBill = response[bill];
-                  if (currentBill['billNumber'] == data['bills'][i]['number']){
-                    billSummary = currentBill['summary'];
-                    break;
+          $.get(sponsorURL, (res) => {
+            billData = res;
+            let sponsorId = res["bill"]["sponsors"][0].bioguideId;
+            let sponsorImgURL =
+              "https://api.congress.gov/v3/member/" +
+              sponsorId +
+              "?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
+            $.get(sponsorImgURL, (r) => {
+              var sponsorImg = r["member"]["depiction"].imageUrl;
+              $.get(billInfoURL, (resp) => {
+                if (data["bills"][i]["latestAction"]["actionDate"]) {
+                  var actionDate = data["bills"][i]["latestAction"]["actionDate"];
+                }
+                let month = actionDate[5] + actionDate[6];
+                if (month[0] == 0) {
+                  month = month[1];
+                }
+                let day = actionDate[8] + actionDate[9];
+                if (day[0] == 0) {
+                  day = day[1];
+                }
+                let year =
+                  actionDate[0] + actionDate[1] + actionDate[2] + actionDate[3];
+                actionDate = month + "/" + day + "/" + year;
+                if (resp["summaries"][0]) {
+                  billSummary = resp["summaries"][0]["text"];
+                } else if (resp["summaries"]) {
+                  billSummary = resp["summaries"]["text"];
+                }
+
+                if (billSummary === undefined) {
+                  color1 = '#6090FF'
+                  color2 = '#FF76A1'
+                  for (let bill in finalResponse) {
+                    let currentBill = finalResponse[bill];
+                    if (currentBill['billNumber'] == data['bills'][i]['number']) {
+                      billSummary = currentBill['summary'];
+                      break;
+                    }
                   }
                 }
-              }
-  
-              let cardHtml =
-                '<div class="card m-3" style="width: auto"> \
+
+                let cardHtml =
+                  '<div class="card m-3" style="width: auto"> \
                 <div class="card-body"><div class=""><div class="d-flex align-items-center">\
                     <a href="/member-desc.html?id=' +
-                r["member"].bioguideId +
-                '"><img src="' +
-                sponsorImg +
-                '" class="sponsor-img" style="object-fit:cover;width: 50px;height: 50px;border-radius: 50%; margin-right: 10px;"></a>\
+                  r["member"].bioguideId +
+                  '"><img src="' +
+                  sponsorImg +
+                  '" class="sponsor-img" style="object-fit:cover;width: 50px;height: 50px;border-radius: 50%; margin-right: 10px;"></a>\
                     <div>' +
-                '<div class="bill-title" style="font-weight: bold;"><h5 class="card-title" style="display:inline;">' +
-                data["bills"][i]["title"] +
-                '</h5>\
+                  '<div class="bill-title" style="font-weight: bold;"><h5 class="card-title" style="display:inline;">' +
+                  data["bills"][i]["title"] +
+                  '</h5>\
                       </div>\
                             <div class="latest-action"><small class="d-inline-flex mb-3 px-2 py-1 fw-semibold text-warning-emphasis bg-warning-subtle border border-warning-subtle rounded-2">LATEST ACTION (' +
-                actionDate +
-                "): " +
-                data["bills"][i]["latestAction"]["text"] +
-                '</small></div></div></div>\
+                  actionDate +
+                  "): " +
+                  data["bills"][i]["latestAction"]["text"] +
+                  '</small></div></div></div>\
                   <p class="card-text mt-2" style="background: linear-gradient(to right, ' + color1 + ', ' + color2 + '); -webkit-background-clip: text; -webkit-text-fill-color: transparent">' +
-                billSummary +
-                '</p> \
+                  billSummary +
+                  '</p> \
                   <span class="badge rounded-pill text-bg-secondary">' +
-                data["bills"][i]["originChamberCode"] +
-                data["bills"][i]["number"] +
-                '</span> \
+                  data["bills"][i]["originChamberCode"] +
+                  data["bills"][i]["number"] +
+                  '</span> \
                   <a id="' +
-                data["bills"][i]["congress"] +
-                "-" +
-                data["bills"][i]["type"].toLowerCase() +
-                "-" +
-                data["bills"][i]["number"] +
-                '" class="btn btn-primary btn-sm ms-3">More info</a>\
+                  data["bills"][i]["congress"] +
+                  "-" +
+                  data["bills"][i]["type"].toLowerCase() +
+                  "-" +
+                  data["bills"][i]["number"] +
+                  '" class="btn btn-primary btn-sm ms-3">More info</a>\
                 </div> \
               </div>';
-  
-              $("#column-1").append(cardHtml);
-              $(
-                "#" +
-                data["bills"][i]["congress"] +
-                "-" +
-                data["bills"][i]["type"].toLowerCase() +
-                "-" +
-                data["bills"][i]["number"]
-              ).on("click", function (e) {
-                let params = e.target.id.split("-");
-                let congressNum = params[0];
-                let billType = params[1];
-                let billNum = params[2];
-                let apiUrl =
-                  "https://api.congress.gov/v3/bill/" +
-                  congressNum +
-                  "/" +
-                  billType +
-                  "/" +
-                  billNum +
-                  "?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
-                $.get(apiUrl, function (resp) {
-                  openBillModal(resp);
+
+                $("#column-1").append(cardHtml);
+                $(
+                  "#" +
+                  data["bills"][i]["congress"] +
+                  "-" +
+                  data["bills"][i]["type"].toLowerCase() +
+                  "-" +
+                  data["bills"][i]["number"]
+                ).on("click", function (e) {
+                  let params = e.target.id.split("-");
+                  let congressNum = params[0];
+                  let billType = params[1];
+                  let billNum = params[2];
+                  let apiUrl =
+                    "https://api.congress.gov/v3/bill/" +
+                    congressNum +
+                    "/" +
+                    billType +
+                    "/" +
+                    billNum +
+                    "?api_key=O4qhb9hRP8dwqw9yr7TPkAUeeJyXGb2Y37ntvfzA";
+                  $.get(apiUrl, function (resp) {
+                    openBillModal(resp);
+                  });
                 });
-              });
-            }
-  
-            );
+              }
+
+              );
+            });
           });
-        });
-      }
-    })
+        }
+      })
   },
   error: function (err) {
     console.error("ERROR: Please try again later. ", err.statusCode());
